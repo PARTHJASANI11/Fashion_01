2665e6be79fe5234b7adf05da4247f7d29874b1d
Parth Jasani <18bce085@nirmauni.ac.in>
2021-01-31 17:24:54 +0530

    Commit on 31/01/2021 at 05:24 pm

--- Fashion_01_01/views.py
+++ Fashion_01_01/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 #from .models import destination
 from django.http import HttpResponse
 
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import random
+
 
 # Create your views here.
 
@@ -30,15 +35,45 @@ def Fashion_Site2_URL(request):
     brand=request.GET['brand']
     color=request.GET['color']
 
-    if(brand != 'Any' or color == 'Any'):
+    opts = webdriver.ChromeOptions()
+    opts.headless = True
+    browser = webdriver.Chrome(options=opts)
+
+    """if(brand != 'Any' or color == 'Any'):
         Url2 = 'https://www.pantaloons.com/c/'+gender+'/t-shirts-188?source=menu&page=1&orderway=desc&orderby=position&fp[]=Sizes__fq:'+size+'%7CSubbrand__fq:'+brand+'&utm_campaign=pure_brand_exact_ao&utm_medium=cpc'
-        return render(request, 'Fashion_Site2_URL.html', {'URL2': Url2})
+        #return render(request, 'Fashion_Site2_URL.html', {'Links2': links})
+
     elif (brand == 'Any' or color != 'Any'):
         Url2 = 'https://www.pantaloons.com/c/' + gender + '/t-shirts-188?source=menu&page=1&orderway=desc&orderby=position&fp[]=Sizes__fq:' + size + '%7CColor__fq:' + color + '&utm_campaign=pure_brand_exact_ao&utm_medium=cpc'
-        return render(request, 'Fashion_Site2_URL.html', {'URL2': Url2})
-    else:
-        Url2='https://www.pantaloons.com/c/'+gender+'/t-shirts-188?source=menu&page=1&orderway=desc&orderby=position&fp[]=Color__fq:'+color+'%7CSizes__fq:'+size+'%7CSubbrand__fq:'+brand+'&utm_campaign=pure_brand_exact_ao&utm_medium=cpc'
-        return render(request,'Fashion_Site2_URL.html', {'URL2': Url2})
+        return render(request, 'Fashion_Site2_URL.html', {'URL2': Url2})"""
+
+    Url2='https://www.pantaloons.com/c/'+gender+'/t-shirts-188?source=menu&page=1&orderway=desc&orderby=position&fp[]=Color__fq:'+color+'%7CSizes__fq:'+size+'%7CSubbrand__fq:'+brand+'&utm_campaign=pure_brand_exact_ao&utm_medium=cpc'
+    browser.get(Url2)
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+
+    re1 = soup.find('div', class_="container-fluid scroll_head_top")
+    re2 = re1.find('div', class_='row')
+    re3 = re2.find('div', attrs={'class': 'col-lg-9 col-md-9 col-sm-8 col-xs-9 category-list-view'})
+    re4 = re3.find('div', attrs={'class': 'filter_content_wrapper'})
+    re_best = re4.find('div', {'class': 'product_search_content row'})
+
+    links = []
+    for one_set in re_best.find_all("div", class_="col-lg-4 col-md-4 col-sm-4 col-xs-4 category-list-col"):
+        a1 = one_set.find("li", class_='slide')
+        if a1:
+            a2 = a1.find("a")
+        if a2:
+            links.append(a2.get('href'))
+    #print(links)
+    browser = webdriver.Chrome(executable_path='chromedriver.exe', options=opts)
+    # browser.get(links[0])
+    return render(request,'Fashion_Site2_URL.html', {'Links2': links, 'Url2': Url2},)
+
+
+
+
+
+
 
 def Fashion_Site3(request):
     return render(request,'Fashion_Site3.html')
